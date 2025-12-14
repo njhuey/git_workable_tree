f983d6d58e8c287589111252499d205a68b6803c
Nathan Huey <nathanhuey44@gmail.com>
2025-12-13 19:58:39 -0800

    Add basic structure of CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "git workabletree",
+    version,
+    about = "
+Git worktrees made workable
+===========================
+
+This CLI is designed to make using git worktrees more intuitive.
+",
+    long_about = None,
+)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    // Clone a git repository and correctly setup the git worktree structure.
+    Clone(CloneArgs),
+
+    // Add a git worktree.
+    Add(AddArgs),
+
+    // Remove a git worktree.
+    Remove(RmArgs),
+}
+
+#[derive(Parser, Debug)]
+struct CloneArgs {
+    repo: String,
+}
+
+#[derive(Parser, Debug)]
+struct AddArgs {
+    branch: String,
+
+    #[arg(short, long)]
+    new: bool,
+}
+
+#[derive(Parser, Debug)]
+struct RmArgs {
+    branch: String,
+
+    #[arg(short, long)]
+    force: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Clone(args) => {
+            println!("Creating Repo")
+        }
+        Commands::Add(args) => {
+            println!("Adding worktree")
+        }
+        Commands::Remove(args) => {
+            println!("Removing worktree")
+        }
+    }
 }
